71d2a5a807c4a3125253903b2b9651ec5760dcc4
YuriBandeira28 <yuri.2021002726@aluno.iffar.edu.br>
2024-08-19 21:06:42 -0300

    feat
    adicionado gráficos diversos

--- supermetrics/app_supermetrics/views.py
+++ supermetrics/app_supermetrics/views.py
@@ -14,20 +14,14 @@ def dashboard(request: HttpRequest):
     
     map = folium.Map(location=[-27.358563288481317, -53.39957273825672], zoom_start=13, height='100%')
     dados_mapa = df_mapa[['latitude', 'longitude', 'total_compras']].values.tolist()
-    HeatMap(dados_mapa, radius=20).add_to(map)
+    HeatMap(dados_mapa, radius=40).add_to(map)
     mapa = map._repr_html_()
     df_relatorio = df_mapa
-    # df_relatorio['cidade_abreviada'] = df_relatorio['munnom'].str.strip(' ')[0]
 
     df_relatorio['cidade_abreviada'] = df_relatorio['munnom'].apply(abrevia_cidade)
     df_relatorio = df_relatorio.sort_values(by='total_compras', ascending=False)
     realtorio_mapa = df_relatorio.to_dict('records')
-    # df_grafico = df_mapa[['trabairro', 'munnom', 'total_compras']]
-    # df_grafico = df_grafico.rename(columns={
-    #     'trabairro': 'Bairro',
-    #     'munnom': 'Cidade',
-    #     'total_compras': "Total de Compras"
-    # })
+
     df_grafico_dias_mes = get_df_compras_dia()
     grafico = cria_grafico_compras_dia_mes(df_grafico_dias_mes)
     df_grafico_pizza = get_df_pizza()
